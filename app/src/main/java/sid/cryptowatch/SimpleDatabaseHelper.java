67c58594d58176c78a13f48b00febcb65c7d9ac5
Siddharth Sahay <sahaysid@gmail.com>
2018-01-25 01:32:31 +0530

    DB onCreate not being called, wow

--- app/src/main/java/sid/cryptowatch/SimpleDatabaseHelper.java
+++ app/src/main/java/sid/cryptowatch/SimpleDatabaseHelper.java
@@ -11,10 +11,10 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class SimpleDatabaseHelper {
-    private SQLiteOpenHelper _openHelper;
-    private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "CryptoWatchSid";
-    private static final String TABLE_NAME_ALERTS = "alerts";
+    private static SQLiteOpenHelper _openHelper;
+    private static final int DATABASE_VERSION = 2;
+    private static final String DATABASE_NAME = "CryptoWatchSid2";
+    private static final String TABLE_NAME_ALERTS = "activealerts";
     private static final String TABLE_NAME_PRICES = "currentprices";
     //private static final String KEY_CURRENCY = "currency";
     private static final String KEY_LAST_PRICE = "last_price";
@@ -24,43 +24,91 @@ public class SimpleDatabaseHelper {
     private static final String KEY_AMOUNT = "amount";
     private static final String KEY_CURRENCY = "currency";
 
+    int mOpenCounter = 0;
+    SQLiteDatabase mDatabase;
+    SQLiteOpenHelper mDatabaseHelper;
 
     public SimpleDatabaseHelper(Context context) {
         _openHelper = new SimpleSQLiteOpenHelper(context);
+        mDatabaseHelper = new SimpleSQLiteOpenHelper(context);
     }
 
     /* This is an internal class that handles the creation of all database tables */
     class SimpleSQLiteOpenHelper extends SQLiteOpenHelper {
         SimpleSQLiteOpenHelper(Context context) {
-            super(context, DATABASE_NAME+".db", null, 1);
+            super(context, DATABASE_NAME+".db", null, DATABASE_VERSION);
         }
 
         @Override
         public void onCreate(SQLiteDatabase db) {
+            db.beginTransaction();
+            /*
+            Stupid bloody Android isn't calling this function no matter what I do.
+            Calling getWritableDatabase() doesn't work
+            Changing version doesn't work
+            Uninstalling doesn't work
+            Makes me wonder how it's been working all this time (and when the f*$% it actually ever called this function)
+            MIND = BLOWN. NOT.
+             */
             String CREATE_TABLE_ALERTS = String.format("create table if not exists %s(%s text not null, %s text not null)",
                     TABLE_NAME_ALERTS, KEY_CURRENCY, KEY_AMOUNT);
             db.execSQL(CREATE_TABLE_ALERTS);
             String CREATE_TABLE_PRICES = String.format("create table if not exists %s(%s text not null, %s text not null)",
                     TABLE_NAME_PRICES, KEY_CURRENCY, KEY_LAST_PRICE);
             db.execSQL(CREATE_TABLE_PRICES);
+            db.endTransaction();
+            //db.close();
         }
 
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            this.onCreate(db);
+        }
+    }
+    public synchronized SQLiteDatabase openDatabase() {
+        mOpenCounter++;
+        if(mOpenCounter == 1) {
+            // Opening new database
+            mDatabase = mDatabaseHelper.getWritableDatabase();
+        }
+        return mDatabase;
+    }
+
+    public synchronized void closeDatabase() {
+        mOpenCounter--;
+        if(mOpenCounter == 0) {
+            // Closing database
+            mDatabase.close();
+
         }
     }
 
-
-    public void updateCurrent(String amt, String coin){
+    public synchronized void updateCurrent(String amt, String coin){
         SQLiteDatabase db = _openHelper.getWritableDatabase();
+        //SQLiteDatabase db = openDatabase();
+        //db.beginTransaction();
         db.execSQL(String.format("delete from %s where %s='%s'", TABLE_NAME_PRICES, KEY_CURRENCY, coin));
         db.execSQL(String.format("insert into %s values('%s', '%s')", TABLE_NAME_PRICES, coin, amt));
-        db.close();
+        //db.endTransaction();
+        //db.close();
     }
-    public float getCurrent(String coin){
+    public synchronized float getCurrent(String coin){
+
         Float lp;
         String lp_;
+
+        //SQLiteDatabase db = _openHelper.getWritableDatabase();
         SQLiteDatabase db = _openHelper.getWritableDatabase();
+        //Cursor c1 = db.rawQuery("PRAGMA journal_mode=OFF", null); c1.close(); // DOES NOT WORK, NOTHING WORKS
+        //db.beginTransaction();
+
+        //The most ridiculous hack ever. Android won't call onCreate no matter what, so here:-
+        String CREATE_TABLE_ALERTS = String.format("create table if not exists %s(%s text not null, %s text not null)",
+                TABLE_NAME_ALERTS, KEY_CURRENCY, KEY_AMOUNT);
+        db.execSQL(CREATE_TABLE_ALERTS);
+        String CREATE_TABLE_PRICES = String.format("create table if not exists %s(%s text not null, %s text not null)",
+                TABLE_NAME_PRICES, KEY_CURRENCY, KEY_LAST_PRICE);
+        db.execSQL(CREATE_TABLE_PRICES);
         Cursor cursor = db.rawQuery(String.format("select distinct %s, %s from %s where %s='%s'", KEY_LAST_PRICE, KEY_CURRENCY, TABLE_NAME_PRICES, KEY_CURRENCY, coin), null);
         if (cursor.getCount() == 0) {
             updateCurrent("0", coin);
@@ -69,14 +117,16 @@ public class SimpleDatabaseHelper {
         cursor.moveToFirst();
         lp_ = cursor.getString(cursor.getColumnIndex(KEY_LAST_PRICE));
         cursor.close();
-        db.close();
+        //db.endTransaction();
+        //db.close();
         System.out.println(lp_);
         lp = Float.parseFloat(lp_);
         return lp;
 
     }
-    public void addAlert(String amt, String currency){
+    public synchronized void addAlert(String amt, String currency){
         SQLiteDatabase db = _openHelper.getWritableDatabase();
+        //db.beginTransaction();
         String token;
         switch(currency){
             case "Bitcoin": token = "BTC"; break;
@@ -88,15 +138,30 @@ public class SimpleDatabaseHelper {
         }
         String sql = String.format("insert into %s values('%s', '%s')", TABLE_NAME_ALERTS, token, amt);
         db.execSQL(sql);
-        db.close();
+        //db.endTransaction();
+        //db.close();
     }
-    public void deleteAlert(String amt, String token){
+    public synchronized void deleteAlert(String amt, String token){
         SQLiteDatabase db = _openHelper.getWritableDatabase();
-        db.execSQL(String.format("delete from %s where %s='%s' and %s='%s';", TABLE_NAME_ALERTS, KEY_AMOUNT, amt, KEY_CURRENCY, token));
+        String amt_;
+        try{
+            amt_ = "" + Integer.parseInt(amt);
+        }catch(Exception e){
+            amt_ = "" + Float.parseFloat(amt); //convert back to int since that's how it's stored in db; not cool - should store as string but "float"
+        }
+        //db.beginTransaction();
+        //db.execSQL(String.format("delete from %s where %s='%s' and %s='%s'", TABLE_NAME_ALERTS, KEY_AMOUNT, amt, KEY_CURRENCY, token));
+        if(db.delete(TABLE_NAME_ALERTS, String.format("%s='%s' and %s='%s';", KEY_AMOUNT, amt_, KEY_CURRENCY, token), null) > 0){
+            System.out.println("It's deleting stuff.");
+        }else{
+            System.out.println("It's NOT deleting stuff.");
+        }
+        //db.endTransaction();
         db.close();
     }
-    public String[] getAllAlerts() {
+    public synchronized String[] getAllAlerts() {
         SQLiteDatabase db = _openHelper.getReadableDatabase();
+        //db.beginTransaction();
         Cursor cursor = db.rawQuery(String.format("select distinct %s, %s from %s", KEY_AMOUNT, KEY_CURRENCY, TABLE_NAME_ALERTS), null);
         if (cursor.getCount() == 0) return new String[0];
 
@@ -110,7 +175,8 @@ public class SimpleDatabaseHelper {
             cursor.moveToNext();
         } /* will return "XRP:100" */
         cursor.close();
-        db.close();
+        //db.endTransaction();
+        //db.close();
         return alarms;
     }
 }
